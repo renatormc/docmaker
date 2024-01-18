0b65e324507f4438d0fdfdd8b5c35f4440e35d26
unknown <renatomartinsrmc@gmail.com>
2024-01-18 12:42:57 -0300

    saving

--- models/midia_otica/model.py
+++ models/midia_otica/model.py
@@ -2,8 +2,8 @@ from docmaker.gui import widgets as wt
 from docmaker import DocModel
 from settings import APPDIR
 
-midia_otica_model = DocModel("Mídia ótica", main_template="midia_otica.odt", 
-                             format="odt", lists_folder=APPDIR / "models/midia_otica/listas")
+midia_otica_model = DocModel("Mídia ótica", main_template="midia_otica.docx", 
+                             format="docx", lists_folder=APPDIR / "models/midia_otica/listas")
 
 midia_otica_model.widgets= [
     [
